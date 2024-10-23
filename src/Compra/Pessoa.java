5d0cc22a048cae4af29a4498ed86e6be03778fd4
JACKSON APARECIDO FARIA OLIVEIRA <jackson.oliveira2@GRANBERY.local>
2024-10-23 19:34:39 -0300

    Segunda parte

--- src/Compra/Pessoa.java
+++ src/Compra/Pessoa.java
@@ -2,9 +2,24 @@ package Compra;
 
 public class Pessoa {
 
-    private String pagamento;
+    private String tipoPagamento;
 
-    public String getPagamento() {
-        return pagamento;
+    public String getTipoPagamento() {
+        return tipoPagamento;
+    }
+
+    public void tipoBoleto() {
+        Transacao transacao = new Transacao();
+        this.tipoPagamento = transacao.pagamento(new PagamentoBoleto());
+    }
+
+    public void tipoPix() {
+        Transacao transacao = new Transacao();
+        this.tipoPagamento = transacao.pagamento(new PagamentoPIX());
+    }
+
+    public void tipoCartao() {
+        Transacao transacao = new Transacao();
+        this.tipoPagamento = transacao.pagamento(new PagamentoCartao());
     }
 }
